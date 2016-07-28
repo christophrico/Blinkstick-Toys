aa2d3fa6c2b0d1de513c19bf97767e3f9b79420a
Diff <burritosaur@gmail.com>
2016-07-28 02:03:21 -0400

    Added fire.py

---
+++ fire.py
@@ -0,0 +1,24 @@
+from blinkstick import blinkstick
+from time import sleep, time
+from math import sin
+from colorsys import hsv_to_rgb
+
+stk = blinkstick.find_first()
+cnt = stk.get_led_count()
+
+speed = 2
+popspeed = 4
+
+while True:
+	data = []
+	for i in range(cnt):
+		x = time()*speed # Base speed control
+		y = x+i # For some variation from LED to LED
+		z = x*popspeed+i**2 # For the sporadic flickering
+		hue = ((sin(y/.4)+sin(y/.2)+sin(y/.5))/6+.5)*.06 # Main color
+		hueplus = max((sin(z)+sin(z/3.0)+sin(z/7.0))/5-.4,0) # Flicker
+		r, g, b = hsv_to_rgb(min(hue+hueplus, .07), 1.0-hueplus, sin(z)/8+.875) # Convert to RGB
+		#print(hueplus, r, g, b)
+		data = data + [int(g*255), int(r*255), int(b*255)] # Convert to GRB and add to the frame.
+	stk.set_led_data(0, data) # Send off to the blinkstick
+	sleep(0.02) # Nap for a bit so we don't overwhelm the blinkstick.
