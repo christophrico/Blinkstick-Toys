0f1b20aec69b32b196d9a939c168e4e95bcf0ba7
Diff <burritosaur@gmail.com>
2016-07-30 00:53:42 -0400

    Added description and information to the top of all scripts, added brightness option.

--- rotatingrainbow.py
+++ rotatingrainbow.py
@@ -1,3 +1,14 @@
+####
+# rotatingrainbow.py by Different55 <burritosaur@protonmail.com>
+# Cycles through a full spectrum of colors and rotates it around all available
+# LEDs on the first channel o the first blinkstick.
+####
+
+speed = 1 # 2 is 2x speed, .5 is half speed, you get the idea.
+fps = 50.0 # Frames per second of the animation. 50 is about the upper limit.
+cut = 2 # how much of the spectrum to show. 1 = full spectrum, 2 = 2 full spectrums, .5 is half a spectrum.
+brightness = 1.0 # Brightness of animation from 0 to 1
+
 from blinkstick import blinkstick
 from time import sleep, time
 from colorsys import hsv_to_rgb
@@ -6,17 +17,11 @@ from math import sin
 stk = blinkstick.find_first()
 cnt = stk.get_led_count()
 
-speed = 1 # 1 is 1x speed, 0 is stopped, you get the idea
-wait = 0.02 # how long to wait between sending data. 0.02 is about the lower limit.
-cut = 2 # how much of the spectrum to show. 1 = full spectrum, 2 = 2 full spectrums, .5 is half a spectrum. good for devices with few LEDs.
-
-data = [0]*96
 while True:
     data = []
     for i in range(1,cnt+1):
-        (r, g, b) = hsv_to_rgb(i/float(cnt*(1.0/cut))+time()*speed, 1, 1)
+        (r, g, b) = hsv_to_rgb(i/float(cnt*(1.0/cut))+time()*speed, 1, brightness)
         data = data + [int(g*255), int(r*255), int(b*255)]
         #print(int(r*255), int(g*255), int(b*255), i/32.0,)
-    #print(data)
     stk.set_led_data(0, data)
-    sleep(wait)
+    sleep(1/fps)
