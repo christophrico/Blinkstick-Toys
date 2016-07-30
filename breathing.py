2e32d455c3d36a86ce2b17a9eef2a42f42293b89
Diff <burritosaur@gmail.com>
2016-07-30 13:09:36 -0500

    Changed breathing.py to use FPS instead of ms

--- breathing.py
+++ breathing.py
@@ -8,7 +8,7 @@ from colour import Color
 # The color is set equally to all available LEDs on the first blinkstick found.
 ####
 
-wait = 0.05 # Time to wait between frames. 0.02 is about the lower limit
+fps = 50.0 # How many frames per second. 50 is about the upper limit.
 colorin = Color('#b71500')
 colorout = Color('black')
 
@@ -24,13 +24,13 @@ while True:
 		last = Color(rgb=((last.red*5+colorin.red)/6, (last.green*5+colorin.green)/6, (last.blue*5+colorin.blue)/6))
 		data = [int(last.green*255), int(last.red*255), int(last.blue*255)] * cnt
 		stk.set_led_data(0, data)
-		sleep(wait)
+		sleep(1/fps)
 		print('breathe in', last)
 	print('pause', last)
-	sleep(wait*10)
+	sleep(1/fps*7)
 	while last.hex != colorout.hex and last.luminance > .004: # For fading to black, this helps a bit since the blinkstick completely blanks out at low brightness levels.
 		last = Color(rgb=((last.red*6+colorout.red)/7, (last.green*6+colorout.green)/7, (last.blue*6+colorout.blue)/7))
 		data = [int(last.green*255), int(last.red*255), int(last.blue*255)] * cnt
 		stk.set_led_data(0, data)
-		sleep(wait)
+		sleep(1/fps)
 		print('breathe out', last)
