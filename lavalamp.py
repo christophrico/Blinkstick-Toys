48f267a8a0e9223fbef936924b7fda2b036218de
Diff <burritosaur@gmail.com>
2016-07-30 00:47:17 -0400

    Added description and information to the top of all scripts, added brightness option.

--- lavalamp.py
+++ lavalamp.py
@@ -1,3 +1,16 @@
+####
+# lavalamp.py by Different55 <burritosaur@protonmail.com>
+# This script smoothly and slowly shifts between many colors. It's not actually
+# anything like a lavalamp, it just kind of reminded me of one.
+####
+
+fps = 50.0 # FPS of the animation. 50 is about the upper limit.
+speed = 1 # Speed of animation
+type = 1 # "1" for hard edges on colors, "1.0" for smooth fading.
+brightness = 1.0 # Brightness of animation, from 0 to 1
+
+### END OPTIONS ###
+
 from blinkstick import blinkstick
 from time import sleep, time
 from colorsys import hsv_to_rgb
@@ -6,17 +19,13 @@ from math import sin
 stk = blinkstick.find_first()
 cnt = stk.get_led_count()
 
-wait = 0.02 # Time to wait between frames. 0.02 is about the lower limit
-speed = 1 # Speed of animation
-type = 1 # "1" for hard edges on colors, "1.0" for smooth fading.
-
 data = [0]*96
 while True:
     data = []
     for i in range(1,cnt+1):
-        (r, g, b) = hsv_to_rgb((sin(time()/(8.0/speed)+i/(4*type))+1)/2, 1, 1)
+        (r, g, b) = hsv_to_rgb((sin(time()/(8.0/speed)+i/(4*type))+1)/2, 1, brightness)
         data= data + [int(g*255), int(r*255), int(b*255)]
         #print(int(r*255), int(g*255), int(b*255))
     #print(data)
     stk.set_led_data(0, data)
-    sleep(wait)
+    sleep(1/wait)
