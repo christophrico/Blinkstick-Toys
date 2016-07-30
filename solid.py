01eb85fe6ed711a2f5c95dbc7fc1fca205a23e08
Diff <burritosaur@gmail.com>
2016-07-30 00:35:39 -0400

    Added description and information to the top of all scripts.

--- solid.py
+++ solid.py
@@ -1,7 +1,12 @@
+####
+# solid.py by Different55 <burritosaur@protonmail.com>
+# This script sets all LEDs on the first blinkstick found to a single solid
+# color. You can change the color by modifying the code below.
+####
+
 from blinkstick import blinkstick
 
 stk = blinkstick.find_first()
-
 cnt = stk.get_led_count()
 
 stk.set_led_data(0, [0, 50, 0] * cnt) # change the color here. Remember, the format is GRB.
